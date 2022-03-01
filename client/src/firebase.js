3f6c189d6b7f10c156c38dbc1847ce6e40c8ea73
evelyn <evelynrhee103@gmail.com>
2022-03-01 04:37:01 -0800

    upgrade dependencies, firebase, react router to v6

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,62 +1,71 @@
-// Dependencies
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/storage';
+//import firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, getDocs, doc, setDoc, getDoc } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+//import 'firebase/storage';
+import { getStorage } from "firebase/storage";
+import 'firebase/analytics'
 
 /**
- * Fine to not gitignore
  * Lets the app know which Google Firebase server the app should talk to
  */
 
 const config = {
-  apiKey: 'AIzaSyCHIn-4BWduDME5PSI07pykoOedhtoDmq8',
-  authDomain: 'hrla35-mvp.firebaseapp.com',
-  databaseURL: 'https://hrla35-mvp.firebaseio.com',
-  projectId: 'hrla35-mvp',
-  storageBucket: 'hrla35-mvp.appspot.com',
-  messagingSenderId: '344292705723',
-  appId: '1:344292705723:web:500ebbcdbaa52b86d4309e',
-  measurementId: 'G-SGQYM7M1QP'
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  // databaseURL: 'https://hrla35-mvp.firebaseio.com',
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
-firebase.initializeApp(config);
+const streakApp = initializeApp(config);
 
 /**
  * Remove this later
  * For debugging purposes
  */
 
-window.firebase = firebase;
+//window.firebase = firebase;
 
 /**
  * Firebase Solutions
  */
 
-export const firestore = firebase.firestore();
-export const storage = firebase.storage();
-export const auth = firebase.auth();
+export const firestore = getFirestore();
+export const storage = getStorage(streakApp);
+export const auth = getAuth();
 
 /**
  * Firebase Packaged Functionality
  */
 
-export const provider = new firebase.auth.GoogleAuthProvider();
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const provider = new GoogleAuthProvider();
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
 
 export const signInWithEmail = (email, password) =>
-  auth.signInWithEmailAndPassword(email, password);
+  signInWithEmailAndPassword(email, password);
+
+export const registerWithEmailAndPassword = ( email, password ) =>{
+  console.log('registering')
+  return createUserWithEmailAndPassword(auth, email, password)
+}
 
 export const resetPasswordWithEmail = (email) =>
   auth.sendPasswordResetEmail(email);
 
-export const signOut = () => auth.signOut();
+  export const signOutOfApp = () => signOut(auth)
 
 /**
  * Generalized Functionality
  */
 
-const getRef = (collection, UID) => firestore.collection(collection).doc(UID);
+const getRef = (collection, UID) => 
+//firestore.collection(collection).doc(UID);
+  doc( firestore, collection, UID )
+
 const performUpdate = async (docRef, updates) =>
   await docRef.update({ ...updates });
 
@@ -66,23 +75,32 @@ const performUpdate = async (docRef, updates) =>
 
 export const createUserProfileDocument = async (user, additionalData) => {
   if (!user) return;
+  console.log('creating profile')
 
   const uRef = getRef('users', user.uid);
 
   try {
-    const uDoc = await uRef.get();
+    //const uDoc = await uRef.get();
+    const uDoc = await getDoc( uRef );
 
     if (!uDoc.exists) {
       const { email, displayName, photoURL } = user;
       const createdAt = new Date();
 
-      await uRef.set({
+      // await uRef.set({
+      //   displayName,
+      //   email,
+      //   photoURL,
+      //   createdAt,
+      //   ...additionalData
+      // });
+      await setDoc( uRef, {
         displayName,
         email,
         photoURL,
         createdAt,
         ...additionalData
-      });
+      } )
     }
 
     return getUserDocument(user.uid);
@@ -328,4 +346,4 @@ export const challengeCheckIn = async (CUID, UID) => {
   }
 };
 
-export default firebase;
+//export default firebase;
